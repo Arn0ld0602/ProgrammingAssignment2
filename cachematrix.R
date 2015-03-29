f836f90c58bdcd41840b7106fe497be90a9661aa
Arn0ld0602 <Arnold.guzman@traxtech.com>
2015-03-29 13:27:29 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix() returns a "matrix" object with the below function list.
+##      These functions return empty values.
+## makeCacheMatrix(x) returns a special "matrix" object which contains a list
+##      of data manipulation functions (see below). These functions are used by
+##      cacheSolve to access and modify the matrix data inverse.
+## Returns list of the following functions: 
+##      $set(y): sets the internal data variable (note the internal data can also
+##              be set by calling makeCacheMatrix(x))
+##      $get(): returns the internal matrix data
+##      $setSolve(solution): sets the cache for the solution (note: no computation 
+##              occurs inside of makeCacheMatrix)
+##      $getSolve(): returns the cached solution value (returns NULL if 
+##              setSolve has not been called for this object instance yet)
 
 makeCacheMatrix <- function(x = matrix()) {
  i <- NULL               ##set inverse of a matrix to null
@@ -27,7 +36,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x), where x is the returned function list from makeCacheMatrix.
+## Returns: inverse of "matrix" x.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
